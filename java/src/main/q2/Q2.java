c753e197dde33b31360755588dd49bbf75637f91
Julian V. Modesto <julianvmodesto@gmail.com>
2016-12-19 22:31:19 -0500

    Do first pass

--- java/src/main/q2/Q2.java
+++ java/src/main/q2/Q2.java
@@ -1,18 +1,61 @@
 package q2;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Q2 {
 
     static List<String> routeAll(List<Route> routes, List<String> paths) {
         List<String> endpoints = new ArrayList<String>();
         // Your code here
+
+        Map<String, String> staticRoutes = new HashMap<>();
+        List<Route> dynamicRoutes = new ArrayList<>();
+        for (Route route : routes) {
+            if (!route.path.contains(("/X"))) {
+                staticRoutes.put(route.path, route.endpoint);
+            } else {
+                dynamicRoutes.add(route);
+            }
+        }
+
+        System.out.println(staticRoutes.toString());
+
+        for (String path : paths) {
+            if (staticRoutes.containsKey(path)) {
+                endpoints.add(staticRoutes.get(path));
+            } else {
+                boolean isFound = false;
+                for (Route dynamicRoute : dynamicRoutes) {
+                    // We have to check all dynamic routes
+                    if (isFound) {
+                        break;
+                    }
+
+                    String[] dynamicRoutePaths = dynamicRoute.path.split("/");
+                    String[] pathParts = path.split("/");
+
+                    if (dynamicRoutePaths.length == pathParts.length) {
+                        for (int i = 0; i < dynamicRoutePaths.length; i++) {
+                            if (!dynamicRoutePaths[i].equals(pathParts[i]) && !dynamicRoutePaths[i].equals("X")) {
+                                break;
+                            }
+                            if (i == dynamicRoutePaths.length - 1) {
+                                isFound = true;
+                                endpoints.add(dynamicRoute.endpoint);
+                            }
+                        }
+                    }
+                }
+                if (!isFound) {
+                    endpoints.add("404");
+                }
+            }
+        }
+
         return endpoints;
     }
 
